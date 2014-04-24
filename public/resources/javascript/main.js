7d2833ee0159df462e1ccdc4e792be4bd827d308
Joel Purra <code+github@joelpurra.com>
2014-04-24 10:31:54 +0200

    Small code cleanups

--- public/resources/javascript/main.js
+++ public/resources/javascript/main.js
@@ -7,6 +7,18 @@ $(function() {
             var instance = createjs.Sound.play(name);
         },
 
+        // http://stackoverflow.com/questions/1219860/html-encoding-in-javascript-jquery
+        // http://stackoverflow.com/a/1219983/907779
+        htmlEncode = function(value) {
+            //create a in-memory div, set it's inner text(which jQuery automatically encodes)
+            //then grab the encoded contents back out.  The div never exists on the page.
+            return $('<div/>').text(value).html();
+        },
+
+        htmlDecode = function(value) {
+            return $('<div/>').html(value).text();
+        },
+
         domainLookupFail = function(jqXHR, textStatus, errorThrown) {
             // TODO: show error to the user
             console.error(jqXHR, textStatus, errorThrown);
@@ -18,22 +30,23 @@ $(function() {
         domainLookupDone = function(data, textStatus, jqXHR) {
             // TODO: use data
             console.log(data, textStatus, jqXHR);
-            
+
             // Load the success or failure image
-            var image="";
-            if (data.isSecure){
-              image="Success";
+            var image = "";
+            if (data.isSecure) {
+                image = "Success";
             } else {
-              image="Failure";
+                image = "Failure";
             }
-            var imageString="<img src=resources/image/"+image+".png />";
+            var imageString = "<img src=resources/image/" + image + ".png />";
             $("#resultImageId").empty();
             $("#resultImageId").append(imageString);
 
             // Load the results text
-            var resultString = "<h2>DNSSEC Results</h1>";
-            resultString += "<ul><li>Domain:  " + data.domain + "</li>";
-            resultString += "<li>DNSSEC Secure:  " + data.isSecure + "</li>";
+            var resultString = "<h2>DNSSEC Results</h2>";
+            resultString += "<ul>";
+            resultString += "<li>Domain:  " + htmlEncode(data.domain) + "</li>";
+            resultString += "<li>DNSSEC Secure:  " + (data.isSecure === true ? "Yes!" : "No!") + "</li>";
             resultString += "</ul>";
             $("#resultId").empty();
             $("#resultId").append(resultString);
@@ -41,11 +54,11 @@ $(function() {
             // Load the tweet button
             var tweetResult = data.domain;
             if (data.isSecure) {
-               tweetResult+=" has successfully implemented #DNSSEC";
+                tweetResult += " has successfully implemented #DNSSEC";
             } else {
-               tweetResult+=" has NOT successfully implemented #DNSSEC";
+                tweetResult += " has NOT successfully implemented #DNSSEC";
             }
-	    var tweetString="<p/><h3><a href=http://twitter.com/home/?status=" + encodeURIComponent(tweetResult) + ">Tweet this result</a></h3>";
+            var tweetString = "<p/><h3><a href=http://twitter.com/home/?status=" + encodeURIComponent(tweetResult) + ">Tweet this result</a></h3>";
             $("#tweetId").empty();
             $("#tweetId").append(tweetString);
 
